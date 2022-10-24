67bd8f767370e760368ab888b90efaa2cd1cca95
Fiona Eckert <fionaeckert@Fionas-MacBook-Air.local>
2022-10-24 18:52:11 -0500

    finalizing magic 8 ball

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const key = process.env.KEY
 //-----------------------------------------------------------------------------------
 var span = document.getElementById('span');
 
@@ -267,20 +266,15 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-var popup = document.getElementById("myPopup");
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': key,
-    'X-RapidAPI-Host': 'magic-8-ball1.p.rapidapi.com'
+function pop() {
+  listOfAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+  var num = Math.floor(Math.random()*listOfAnswers.length);
+
+    const question = prompt("Ask a question to receive a fortune", "Will I find love?");
+    if(typeof question === 'string'){
+      alert(listOfAnswers[num])
+    }
   }
-};
-const getAFortune = () => {
-  fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/?question=I%20will%20succeed%20%3F', options)
-    .then(response => response.json())
-    .then(response => {
-      popup.innerText = response.answer
-    })}
 //-----------------------------------------------------------------------------------
     let project3Folder = dragElement(document.getElementById("project3Div"));
 
@@ -367,3 +361,4 @@ const getAFortune = () => {
       document.onmousemove = null;
     }
   }
+  //-----------------------------------------------------------------------------------
