ff817dade498776dc098829619daeba9e5856023
Fiona Eckert <fionaeckert@Fionas-MacBook-Air.local>
2022-10-23 20:25:24 -0500

    adding in pop-up about me and eight ball

--- portfolio.js
+++ portfolio.js
@@ -345,3 +345,122 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+//-----------------------------------------------------------------------------------
+let aboutMeImage = dragElement(document.getElementById("aboutMeDiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Get the modal
+var modal = document.getElementById("aboutMeModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("aboutMeImage");
+var modalImg = document.getElementById("aboutMePopUp");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var div = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+div.onclick = function() {
+  modal.style.display = "none";
+}
+//-----------------------------------------------------------------------------------
+let eightBallDiv = dragElement(document.getElementById("eightBallDiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+var popup = document.getElementById("myPopup");
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd02dc84b57mshedc2b1f6614dd8ap18fc18jsn952a46505a27',
+    'X-RapidAPI-Host': 'magic-8-ball1.p.rapidapi.com'
+  }
+};
+const getAFortune = () => {
+  fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/?question=I%20will%20succeed%20%3F', options)
+    .then(response => response.json())
+    .then(response => {
+      popup.innerText = response.answer
+    })}
